eaf2c57124bee2f647254ab29fdd3ec9639b785a
Hieu H Vu <hieuvu927@gmail.com>
2021-10-08 22:19:00 -0500

    coursePreq

--- Data/controllers/user.controllers.js
+++ Data/controllers/user.controllers.js
@@ -1,6 +1,6 @@
 const {User} = require("../models")
 
-const getAllUsers = async (req, res) =>{
+const getAllUsers = async (req, res) => {
 	try {
 		const usersList = await User.findAll();
 		res.status(200).send(usersList)
