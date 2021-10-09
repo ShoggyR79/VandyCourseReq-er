09ce725c149c35d755815eed8e37c5073789e7d9
Tiger Li <tiger01tgr@gmail.com>
2021-10-09 14:38:58 -0500

    added stuff to graph.js

--- Graph/graph.js
+++ Graph/graph.js
@@ -1,33 +1,48 @@
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 const fs = require('fs')
 
 class Graph {
 
     constructor(){
         let address = "API/course_file.csv";
+        this.prereqs = [];
+        
+        
+        this.prereqs = [
+            ["1101", "-1"],
+            ["1104", "-1"],
+            ["2212", "1101/1104", "-1"],
+            ["2201", "1101/1104", "-1"],
+            ["3250", "2201/2212", "-1"]
+        ];
 
-        this.prereqs = {
-            "1101": ["-1"],
-            "1104": ["-1"],
-            "2212": ["1101/1104", "-1"],
-            "2201": ["1101/1104", "-1"],
-            "3250": ["2201/2212", "-1"]
-        };
         
         this.taken = ["base"];
         this.unclicked = [];
         this.graph = {"base": []};
     }
+    unclick(id){
+        this.unclicked.push(id);
+    }
+    getPrereqs(){
+        return this.prereqs;
+    }
+    addClass(id){
+        this.taken.push(id);
+        let preCourses = [];
+        for(let i = 0; i < this.prereqs[id].length; ++i){
+
+        }
+    }
     availableClasses(){
         var classList = [];
-        for(key in this.prereqs){
-            if(this.taken.includes(key))
+        for(var i = 0; i < this.prereqsSize; ++i){
+            if(this.taken.includes(this.prereqs[i][0]))
                 continue;
         
-            for(var j = 1; j < this.prereqs[key].length; ++j){
-                let currID = this.prereqs[key][j];
-                if(currID == "-1"){
-                    classList.push(key);
+            for(var j = 1; j < this.prereqs[i].length; ++j){
+                let currID = this.prereqs[i][j];
+                if(currID == -1){
+                    classList.push(this.prereqs[i][0]);
                     break;
                 }
                 
@@ -42,31 +57,24 @@ class Graph {
         }
         return classList;
     }
-    /*
-    addClass(id){
-        this.taken.push(id);
-        let preCourses = [];
-        for(let i = 0; i < this.prereqs[id].length; ++i){
-            
-        }
-    }
-    getPrereqs(){
-        return this.prereqs;
-    }
-    unclick(id){
-        this.unclicked.push(id);
-    }
-    */
 
 }
 
-let graph = new Graph();
-console.log(graph.availableClasses());
+let filename = "API/course_file.txt";
+var text = fs.readFileSync(filename, "utf-8");
+console.log(text);
+// var graph = new Graph(string);
 
 
 /*
-let filename = "API/course_file.csv";
-var text = fs.readFileSync(filename);
-var textByLine = text.split("\n");
-console.log(text);
-*/
+for(var i = 0; i < graph.availableClasses().length; ++i){
+    console.log(graph.availableClasses()[i]);
+}
+
+
+graph.addClass("1101");
+
+
+for(var i = 0; i < graph.availableClasses().length; ++i){
+    console.log(graph.availableClasses()[i]);
+}*/
