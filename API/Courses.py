441ca04064be1cb13d89f0a2506963c87cfd7af0
binhho2607 <binhho2607@gmail.com>
2021-10-09 12:57:09 -0500

    scraping methods

--- API/Courses.py
+++ API/Courses.py
@@ -19,51 +19,51 @@ class Courses:
         return self.weight
 
     map = {
-        1101: "Core",
-        1104: "Core",
-        2201: "Core",
-        2212: "Core",
-        2123: "Core",
-        3251: "Core",
-        3281: "Core",
-        3250: "Core",
-        3270: "Core",
+        "1101": "Core",
+        "1104": "Core",
+        "2201": "Core",
+        "2212": "Core",
+        "2123": "Core",
+        "3251": "Core",
+        "3281": "Core",
+        "3250": "Core",
+        "3270": "Core",
 
-        2231: "Hardware",
-        3276: "Hardware",
-        3282: "Hardware",
-        4284: "Hardware",
+        "2231": "Hardware",
+        "3276": "Hardware",
+        "3282": "Hardware",
+        "4284": "Hardware",
 
-        3253: "Software Engineering/Design",
-        3254: "Software Engineering/Design",
-        3274: "Software Engineering/Design",
-        4278: "Software Engineering/Design",
-        4279: "Software Engineering/Design",
+        "3253": "Software Engineering/Design",
+        "3254": "Software Engineering/Design",
+        "3274": "Software Engineering/Design",
+        "4278": "Software Engineering/Design",
+        "4279": "Software Engineering/Design",
 
-        3258: "Graphics/Animation/VR",
-        3259: "Graphics/Animation/VR",
-        4249: "Graphics/Animation/VR",
+        "3258": "Graphics/Animation/VR",
+        "3259": "Graphics/Animation/VR",
+        "4249": "Graphics/Animation/VR",
 
-        3262: "ML/AI",
-        4260: "ML/AI",
-        4262: "ML/AI",
-        4267: "ML/AI",
-        4269: "ML/AI",
+        "3262": "ML/AI",
+        "4260": "ML/AI",
+        "4262": "ML/AI",
+        "4267": "ML/AI",
+        "4269": "ML/AI",
 
-        3265: "Data",
-        4266: "Data",
+        "3265": "Data",
+        "4266": "Data",
 
-        3860: "Research",
-        3861: "Research",
+        "3860": "Research",
+        "3861": "Research",
 
-        4277: "Cybersecurity",
-        4285: "Cybersecurity",
+        "4277": "Cybersecurity",
+        "4285": "Cybersecurity",
 
-        4283: "Network/Cloud",
-        4287: "Network/Cloud",
-        4288: "Network/Cloud",
+        "4283": "Network/Cloud",
+        "4287": "Network/Cloud",
+        "4288": "Network/Cloud",
 
-        4959: "Computer Science Seminar",
+        "4959": "Computer Science Seminar",
 
-        3252: "Theory"
+        "3252": "Theory"
     }
