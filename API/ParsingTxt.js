c2b960e69733a0cfb3a3924d1389b483593d9289
binhho2607 <binhho2607@gmail.com>
2021-10-09 17:27:40 -0500

    csv export edit the or cases

--- API/ParsingTxt.js
+++ API/ParsingTxt.js
@@ -38,21 +38,24 @@ function buildCourseList(text){
     var startIndex = 0;
     var i=0;
     while(text[startIndex]== "1" || text[startIndex]== "2" || text[startIndex]== "3" || text[startIndex]== "4"){ //check if end of file is not reached
-        var name = text.slice(startIndex,startIndex + 4);
-        prereqList[i] = [];
-        prereqList[i].push(name);
-        name = text.slice(text.slice(startIndex).find("\""),text.slice(startIndex+1).find("\"")+1)
-        startIndex = text.slice(startIndex+1).find("\"")+1
-        name = text.slice(text.slice(startIndex).find("\""),text.slice(startIndex+1).find("]")+1)
+        var id = text.slice(startIndex,startIndex + 4);
+        var name = text.slice(text.slice(startIndex).search("\"")+startIndex  ,  text.slice(text.slice(startIndex).search("\"")+startIndex+1).search("\"")+1+startIndex+1);
+        console.log(text.slice(text.slice(startIndex).search("\"")+startIndex+1).search("\"")+1+startIndex+1)
+        startIndex = text.slice(startIndex+1).search("\"")+1+startIndex;
+        var description = text.slice(text.slice(startIndex).search("\"")+startIndex+1,text.slice(startIndex+1).search("]")+1+startIndex+1);
+        startIndex = text.slice(startIndex+1).search("]")+3+startIndex;
+        var term = text.slice(startIndex,text.slice(startIndex).search(",")+startIndex);
+        startIndex = text.slice(startIndex).search(",")+startIndex;
+        var cat = text.slice(startIndex+1,text.slice(startIndex).search("\n")+startIndex);
+        courseList[id] = [name, cat, description, term];
+        startIndex = text.slice(startIndex+1).search("\n")+1+startIndex+1;
         
-        
-        
-        prereqList[i].push("-1")
-        startIndex = text.slice(startIndex+1).search("\n")+1+startIndex+1; //start of a new line
-        
-        i++;
     }
+    return courseList;
 }
+test = {}
+test = buildCourseList(text);
+//console.log(test)
 
 
 
